da1741cd926b63c24be7480740c1264ab7ca18b1
MudMoh <mudesir@gmail.com>
2025-09-30 15:01:59 +0300

    task 2, 4, and 5 added

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -4,9 +4,9 @@ export namespace Subjects {
   export class Subject {
     teacher: Subjects.Teacher;
 
-    setTeacher(teacher: Subjects.Teacher): void {
+    setTeacher(teacher: teacher.Teacher): void {
       this.teacher = teacher;
     }
   }
-}   //
+}   // Added namespace to encapsulate Subject class and avoid naming conflicts
 
