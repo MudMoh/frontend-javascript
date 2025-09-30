9b8a48efdb0130e57974bdc815ece2435c3c3905
MudMoh <mudesir@gmail.com>
2025-09-30 15:28:09 +0300

    task 2, 4, and 5 added

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -1,13 +1,17 @@
 import { Subject } from './Subject';
 import { Teacher } from './Teacher';
+// task_4/js/subjects/React.ts
 
+declare interface Teacher {
+  experienceTeachingReact?: number;
+}
 export class React extends Subject {
   getRequirements(): string {
     return 'Here is the list of requirements for React';
   }
 
   getAvailableTeacher(): string {
-    if (this.teacher && (this.teacher as Teacher).experienceTeachingReact? number((this.teacher as Teacher).experienceTeachingReact) : 0  ) {
+    if (this.teacher && (this.teacher as Teacher).experienceTeachingReact) {
       return `Available Teacher: ${(this.teacher as Teacher).firstName}`;
     } else {
       return 'No available teacher';
